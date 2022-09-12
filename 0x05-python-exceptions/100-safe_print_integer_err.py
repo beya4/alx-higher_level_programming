dd573f9aa79d6c96b0d3a08f9ec9d1368954f84b
behailu <behailuis4simachew@gmail.com>
2022-09-12 11:29:37 -0700

    bug fixed

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-from sys import stderr
+
+import sys
 
 
 def safe_print_integer_err(value):
+    """Prints an integer
+    Args:
+        value (int): Integer to print.
+    Returns:
+          If value is an integer - True.
+          Otherwise - False.
+    """
     try:
         print("{:d}".format(value))
-    except ValueError as err:
-        stderr.write('Exception: ')
-        stderr.write(str(err))
-        stderr.write('\n')
+        return True
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
-    return True
