8e80dbe5fe8dbf9e4db42f10ba0292850196c6cc
behailu <behailuis4simachew@gmail.com>
2022-09-28 11:54:03 -0700

    just fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
+"""Add all arguments to a Python list and save them to a file."""
+import sys
 
-"""File Json Module."""
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-
-def add_item():
-    """Add all arguments to a Python list, and then save them to a file."""
-    _len = len(argv)
-    mylist = []
     try:
-        mylist = load_from_json_file("add_item.json")
-    except Exception:
-        save_to_json_file(mylist, "add_item.json")
-    if _len < 2:
-        return
-    for i in range(1, _len):
-        mylist.append(argv[i])
-
-    save_to_json_file(mylist, "add_item.json")
-
-
-add_item()
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+        items.extend(sys.argv[1:])
+        save_to_json_file(items, "add_item.json")
