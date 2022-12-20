2fb5a7c835c482d99d08c8b643e775ca1f74bcd9
behailu <behailuis4simachew@gmail.com>
2022-12-20 09:52:52 -0800

    bug fixed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep iF 'Content-Length:' | cut -d ' ' -f 2
+# script that takes in a URL, sends a request to that URL,
+curl -so /dev/null "$1" -w '%{size_download}\n'
