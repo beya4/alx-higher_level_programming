7a50959ed737d6dba3d14b4830df38c165473587
behailu <behailuis4simachew@gmail.com>
2022-12-20 10:10:58 -0800

    yes

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that was a fun effort in breaking to http protocols on holberton servers
-curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond
+curl -sL 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "Origin:You got me!"
