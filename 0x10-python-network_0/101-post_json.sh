2241b20b6840363038a80f2e17f50e2476072a21
behailu <behailuis4simachew@gmail.com>
2022-12-20 10:08:55 -0800

    yes

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+# sends a JSON POST request to a URL passed as the first argument
+curl -H "Content-Type: application/json" -LsX POST "$1" -d "@$2"
